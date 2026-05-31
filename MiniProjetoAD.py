57a4add0feaa03bd8612418a49723a428069d224
ValterF26 <valterf1@live.com>
2026-05-31 11:19:17 -0300

    Carregando e organizando o data base
    Carregamento e leitura do data base e organizando para leitura e limpeza

--- MiniProjetoAD.py
+++ MiniProjetoAD.py
@@ -1,7 +1,13 @@
+# Importando Biblíotecas 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Base_Varejo.csv')
+# Carregando base de dados para visualização
+# Leitura com tratamento para que cada informação de linha fique sob sua coluna mantendo a visualização organizada
+
+df = pd.read_csv('Base_Varejo.csv', sep=';')
 
 print(df.head(5))
+print(df.describe())
